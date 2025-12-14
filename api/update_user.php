3509f2df08ee66c8f2197775975b6535d3e01893
Trannie47 <tranghuyen2012003@gmail.com>
2025-12-15 00:07:35 +0700

    s

--- api/update_user.php
+++ api/update_user.php
@@ -5,7 +5,7 @@ $id    = $_POST['id'];
 $name  = $_POST['name'];
 $email = $_POST['email'];
 
-$sql = "UPDATE users SET name=?, email=? WHERE id=?";
+$sql = "UPDATE Students SET name=?, email=? WHERE id=?";
 $stmt = $conn->prepare($sql);
 $stmt->execute([$name, $email, $id]);
 
--- config/database.php
+++ config/database.php
@@ -1,10 +1,10 @@
 <?php
-
-$host = getenv("DB_HOST");
-$port = getenv("DB_PORT");
-$db   = getenv("DB_NAME");
-$user = getenv("DB_USER");
-$pass = getenv("DB_PASS");
+$getenv = parse_ini_file(__DIR__ . "/../.env");
+$host = $getenv("DB_HOST");
+$port = $getenv("DB_PORT");
+$db   = $getenv("DB_NAME");
+$user = $getenv("DB_USER");
+$pass = $getenv("DB_PASS");
 $conn = new PDO(
     "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
     $user,
