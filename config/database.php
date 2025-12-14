1a72426c4f5ee6a8832021d6fbcc0ac78ba2dd06
Trannie47 <tranghuyen2012003@gmail.com>
2025-12-15 00:11:10 +0700

    Ok

--- config/database.php
+++ config/database.php
@@ -1,10 +1,19 @@
 <?php
-$getenv = parse_ini_file(__DIR__ . "/../.env");
-$host = $getenv("DB_HOST");
-$port = $getenv("DB_PORT");
-$db   = $getenv("DB_NAME");
-$user = $getenv("DB_USER");
-$pass = $getenv("DB_PASS");
+// ===== LOAD .env FILE =====
+$envFile = __DIR__ . '/../.env';
+
+if (!file_exists($envFile)) {
+    die('Missing .env file');
+}
+
+$env = parse_ini_file($envFile);
+
+$host = $env['DB_HOST'];
+$port = $env['DB_PORT'];
+$db   = $env['DB_NAME'];
+$user = $env['DB_USER'];
+$pass = $env['DB_PASS'];
+
 $conn = new PDO(
     "mysql:host=$host;port=$port;dbname=$db;charset=utf8",
     $user,
